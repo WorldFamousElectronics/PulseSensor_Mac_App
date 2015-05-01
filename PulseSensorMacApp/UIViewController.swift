60633e115afcbcb044ddd555d3f73d32cc084ab8
samuraipapa <yury.gitman@gmail.com>
2015-05-01 12:26:58 -0400

    Alter Design: to show Arduino's calculated BPM in comparison to Mac's calculated BPM

---
+++ PulseSensorMacApp/UIViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController.swift
+//  Pulse Sensor
+//
+//  Created by GrownYoda on 4/12/15.
+//  Copyright (c) 2015 World Famous Electronics llc. All rights reserved.
+//
+
+import Foundation
+
+class UIViewController {
+    
+   // <#properties and methods#>
+}
